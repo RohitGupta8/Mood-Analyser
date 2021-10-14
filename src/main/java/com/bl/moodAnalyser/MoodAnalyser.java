593028b247f10c360d18a2304387c5d7b20e01e4
RohitGupta8 <therohitg@gmail.com>
2021-10-14 13:20:17 +0530

    Added : Given null mood return happy

--- src/main/java/com/bl/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/bl/moodAnalyser/MoodAnalyser.java
@@ -9,11 +9,34 @@ package com.bl.moodAnalyser;
  */
 
 public class MoodAnalyser {
-    public static String analyseMood(String message) {
-        if (message.toLowerCase().contains("sad")) {
-            return "Sad";
+    public static String message;
+
+    // Constructor
+    public MoodAnalyser(String message) {
+        MoodAnalyser.message = message;
+        analyseMood();
+    }
+
+    //Getter
+    public String getMessage() {
+        return message;
+    }
+
+    //Setter
+    public void setMessage(String message) {
+        MoodAnalyser.message = message;
+    }
+
+    public static String analyseMood() {
+        try {
+            if (message.toLowerCase().contains("sad")) {
+                return "Sad";
+            } else {
+                return "Happy";
+            }
+        } catch (NullPointerException e) {
+            return "Happy";
         }
-        return "Happy";
     }
 }
 
