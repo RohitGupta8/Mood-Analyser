cebc493585175d916db86c6e6b2794d3ec561548
RohitGupta8 <therohitg@gmail.com>
2021-10-14 14:14:02 +0530

    Added : Test case to given null mood should throw mood analysis exception

--- src/test/java/com/bl/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bl/moodAnalyser/MoodAnalyserTest.java
@@ -4,25 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
     @Test
-    public void givenMessageWhenProperShouldReturnSad() {
-        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
+    public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        moodAnalyser.setMessage("I am in Sad Mood");
         String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
 
     @Test
-    public void givenMessageWhenProperShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+    public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        moodAnalyser.setMessage("I am in happy Mood");
         String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Happy", actualResult);
     }
 
     @Test
-    public void givenMessageNullShouldReturnExceptionHandled() {
-        MoodAnalyser moodanalyser = new MoodAnalyser(null);
-        String actualResult = MoodAnalyser.analyseMood();
-        Assert.assertEquals("Exception Handled", actualResult);
+    public void givenMessage_Null_ShouldReturnMessage() throws MoodAnalysisException {
+        MoodAnalyser moodanalyser = new MoodAnalyser();
+        moodanalyser.setMessage(null);
+        try {
+            String actualResult = MoodAnalyser.analyseMood();
+            Assert.assertEquals("Entered Invalid Mood ", actualResult);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
