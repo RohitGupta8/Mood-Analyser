886222dffc7f5d357254379bc7baf87e5713b1f8
RohitGupta8 <therohitg@gmail.com>
2021-10-14 12:45:48 +0530

    Added : To analyze sad mood of the user

---
+++ src/main/java/com/bl/moodAnalyser/MoodAnalyser.java
@@ -0,0 +1,15 @@
+package com.bl.moodAnalyser;
+
+/**
+ * Purpose  - “I am in Sad Mood” message Should Return SAD
+ *
+ * @author - Rohit Gupta
+ * @version - 17.0
+ * @since - 2021-10-14
+ */
+
+public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        return "Sad";
+    }
+}
