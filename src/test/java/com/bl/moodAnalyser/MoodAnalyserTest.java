9fcfdf9d6638abdd23c4e7f0018c293ca5e00064
RohitGupta8 <therohitg@gmail.com>
2021-10-14 13:21:12 +0530

    Added : Test case Given null mood return happy

--- src/test/java/com/bl/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bl/moodAnalyser/MoodAnalyserTest.java
@@ -7,16 +7,23 @@ public class MoodAnalyserTest {
     // Test case to analyze sad mood of the user
     @Test
     public void givenMessageWhenProperShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String actualResult = MoodAnalyser.analyseMood("User is Sad");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
 
     // Test case to analyze happy mood of the user
     @Test
     public void givenMessageContainsAnyInitShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser();
-        String actualResult = MoodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Happy Mood");
+        String actualResult = MoodAnalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
+
+    @Test
+    public void givenMessage_Null_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Happy", actualResult);
     }
 }
