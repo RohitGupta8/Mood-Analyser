cb63b4156481c1f63786c55fc32b86b2fd51876b
RohitGupta8 <therohitg@gmail.com>
2021-10-14 13:30:17 +0530

    Added : Ability to analyse and respond happy or sad mood

--- src/main/java/com/bl/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/bl/moodAnalyser/MoodAnalyser.java
@@ -9,34 +9,19 @@ package com.bl.moodAnalyser;
  */
 
 public class MoodAnalyser {
-    public static String message;
-
-    // Constructor
-    public MoodAnalyser(String message) {
-        MoodAnalyser.message = message;
-        analyseMood();
-    }
-
-    //Getter
-    public String getMessage() {
-        return message;
-    }
-
-    //Setter
-    public void setMessage(String message) {
-        MoodAnalyser.message = message;
-    }
-
-    public static String analyseMood() {
-        try {
-            if (message.toLowerCase().contains("sad")) {
-                return "Sad";
-            } else {
-                return "Happy";
-            }
-        } catch (NullPointerException e) {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("happy")) {
             return "Happy";
         }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String mood = MoodAnalyser.analyseMood("User is Happy");
+        System.out.println(mood);
+        mood = MoodAnalyser.analyseMood("User is Sad");
+        System.out.println(mood);
     }
 }
-
